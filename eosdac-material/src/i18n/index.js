1eb46e8446ffce11dc2111a6af208475aa39e6a2
Luke Stokes <luke.stokes@gmail.com>
2018-08-17 15:54:20 -0500

    Adding English UK (to be updated)

--- eosdac-material/src/i18n/index.js
+++ eosdac-material/src/i18n/index.js
@@ -1,8 +1,10 @@
 import enUS from './en-us'
+import enUK from './en-uk'
 import de from './de'
 import zhHANS from './zh-hans'
 export default {
   'en-us': enUS,
+  'en-uk': enUK,
   'de': de,
   'zh-hans': zhHANS
 }
