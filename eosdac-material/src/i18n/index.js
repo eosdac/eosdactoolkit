e5b1f750f3c20a738ed88e9931e4c16cdf364481
Luke Stokes <luke.stokes@gmail.com>
2018-08-17 16:17:31 -0500

    Adding French and Spanish (Google Translate version)

--- eosdac-material/src/i18n/index.js
+++ eosdac-material/src/i18n/index.js
@@ -1,5 +1,7 @@
 import enUS from './en-us'
 import enUK from './en-uk'
+import es from './es'
+import fr from './fr'
 import de from './de'
 import it from './it'
 import ro from './ro'
@@ -7,6 +9,8 @@ import zhHANS from './zh-hans'
 export default {
   'en-us': enUS,
   'en-uk': enUK,
+  'es': es,
+  'fr': fr,
   'de': de,
   'it': it,
   'ro': ro,
