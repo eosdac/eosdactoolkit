fac2f086f4b52b7a4bed44628e10f714e36a8950
Luke Stokes <luke.stokes@gmail.com>
2018-08-17 15:59:51 -0500

    Adding Romanian (Google Translate version)

--- eosdac-material/src/i18n/index.js
+++ eosdac-material/src/i18n/index.js
@@ -1,10 +1,12 @@
 import enUS from './en-us'
 import enUK from './en-uk'
 import de from './de'
+import ro from './ro'
 import zhHANS from './zh-hans'
 export default {
   'en-us': enUS,
   'en-uk': enUK,
   'de': de,
+  'ro': ro,
   'zh-hans': zhHANS
 }
