b85df5862c0958f05a7a5d90a43f8e9a64322694
Luke Stokes <luke.stokes@gmail.com>
2018-08-17 16:05:09 -0500

    Adding Italian (Google Translate version)

--- eosdac-material/src/i18n/index.js
+++ eosdac-material/src/i18n/index.js
@@ -1,12 +1,14 @@
 import enUS from './en-us'
 import enUK from './en-uk'
 import de from './de'
+import it from './it'
 import ro from './ro'
 import zhHANS from './zh-hans'
 export default {
   'en-us': enUS,
   'en-uk': enUK,
   'de': de,
+  'it': it,
   'ro': ro,
   'zh-hans': zhHANS
 }
