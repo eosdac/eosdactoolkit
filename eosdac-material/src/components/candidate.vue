3f51ab95ff6f054bcf4b2875e15884e821d61aa9
kasperfish <kasperkasperfish@gmail.com>
2018-08-20 16:21:37 +0200

    fix profile url bug

--- eosdac-material/src/components/candidate.vue
+++ eosdac-material/src/components/candidate.vue
@@ -1,11 +1,11 @@
 <template>
 <div>
 
-<q-item class="bg-dark2 q-px-lg cursor-pointer q-mb-md shadow-5 round-borders" v-bind:class="{ selected_candidate: data.selected }" style="height:80px;">
+<q-item class="bg-dark2 q-px-lg cursor-pointer q-mb-md shadow-5 round-borders" v-bind:class="{ selected_candidate: data.selected }" style="height:80px;" >
   <q-item-side>
     <div class="row">
       <q-btn class="q-mr-md" icon="icon-plus" round color="primary" style="height:55px;width:55px;margin-top:3px;" @click="$emit('clickvotefor')" />
-      <img style="height:60px;width:60px;border-radius:50%;" class="q-mr-md" :src="image_profile" @click="getProfileData">
+      <img style="height:60px;width:60px;border-radius:50%;" class="q-mr-md" :src="image_profile" @click="getProfileData" :name="data.candidate_name+'_pic'">
     </div>
   </q-item-side>
   <q-item-main @click.native="">
@@ -75,9 +75,14 @@ export default {
   },
   methods: {
     getProfileData(){
+      if(this.data.profile !== undefined){
+        this.image_profile = this.data.profile.image;
+        console.log('profile already fetched');
+        return false;
+      }
 
-      if(this.data.bio.substring(0,4) != 'http' || this.data.profile !== undefined ){
-          console.log('no profile link or already fetched')
+      if(this.data.bio.substring(0,4) != 'http' ){
+          console.log('no profile available')
           return false;
       }
       this.$axios.get(this.data.bio).then(r => {
