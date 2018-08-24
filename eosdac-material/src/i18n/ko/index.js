db933d22d75a15a1ddb22e99d80a4d74ddd020c8
Soyoung Kim <36679144+KoyoungK@users.noreply.github.com>
2018-08-24 13:56:20 +0200

    Update index.js
    Translation change for 'switch contrast'.

--- eosdac-material/src/i18n/ko/index.js
+++ eosdac-material/src/i18n/ko/index.js
@@ -1,7 +1,7 @@
 export default {
   constitution: {
     loading_constitution: '헌법 불러오는 중',
-    switch_contrast: '스위치 대조',
+    switch_contrast: '테마색 ',
   },
   default: {
     account_name: '계정 이름',
