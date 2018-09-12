f6d42f980f05e9182aa97adac5ac91dc2059ca65
kasperfish <kasperkasperfish@gmail.com>
2018-09-12 16:40:07 +0200

    added interval loader to progress bar

--- eosdac-material/src/components/voting-progress.vue
+++ eosdac-material/src/components/voting-progress.vue
@@ -3,6 +3,8 @@
     <span class="uppercase">{{ $t("vote_custodians.voting_progress") }} <span class="text-dimwhite on-right on-left"> {{ voting_progress }}%</span></span>
     <span class="q-body-1 text-dimwhite text-italic">(The DAC will be activated at 15%)</span>
     <q-progress animate stripe class="round-borders votingprogress_bar q-mt-md" :style="{height: height}" color="positive" :percentage="voting_progress" />
+    <span v-if="!loading" class="q-caption">update in {{update_in_seconds}} seconds</span>
+    <span v-else class="q-caption">loading...</span>
   </div>
 </template>
 
@@ -16,7 +18,9 @@ export default {
   },
   data () {
     return {
-      voting_progress : 0
+      voting_progress : 0,
+      update_in_seconds:0,
+      loading : false
       
     }
   },
@@ -24,18 +28,36 @@ export default {
   methods:{
     //get voting progress from chain
     async getContractState() {
+      this.loading = true;
       let totalsupply = this.$configFile.network.tokenContract.totalSupply;
       let state = await this.$store.dispatch('api/getContractState', {contract: this.$configFile.network.custodianContract.name})
       if(state){
         this.voting_progress = state.total_weight_of_votes/totalsupply/100;
       }
+      this.loading = false;
 
     },
+    intervaller (i){
+      let oldi = i;
+      let int = setInterval(() => {
+        this.update_in_seconds = i;
+        i--;
+        if(i == -1){
+          clearInterval(int);
+          console.log('xxxx');
+          this.getContractState();
+          this.intervaller(oldi);
+          // this.getContractState();
+        }
+      }, 1000);
+
+    }
 
   },
   mounted(){
-    this.getContractState()
-
+    this.getContractState();
+    //update every 60 seconds
+    this.intervaller(60);
   }
 
 }
