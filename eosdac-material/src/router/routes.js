0ef03da56107bb094badf0b672720b27fbbe1adf
kasperfish <kasperkasperfish@gmail.com>
2018-09-20 20:21:28 +0200

    added router guards in it's own scope

--- eosdac-material/src/router/routes.js
+++ eosdac-material/src/router/routes.js
@@ -1,21 +1,22 @@
 import store from '../store'
 
-function logInCheck (to, from, next) {
-  if (!store.getters['account/getAccountName']) {
-    next({ path: '' })
-  } else {
-    next()
+const Guards = {
+  logInCheck (to, from, next) {
+    if (!store.getters['account/getAccountName']) {
+      next({ path: '' })
+    } else {
+      next()
+    }
+  },
+  custodianCheck (to, from, next) {
+    let roles = store.getters['account/getMemberRoles'];
+    if (!roles.custodian) {
+      next({ path: '' })
+    } else {
+      next()
+    }
   }
-}
-
-function custodianCheck (to, from, next) {
-  let roles = store.getters['account/getMemberRoles'];
-  if (!roles.custodian) {
-    next({ path: '' })
-  } else {
-    next()
-  }
-}
+};
 export default [
   {
     // path: '/:lang',
@@ -23,16 +24,16 @@ export default [
     component: () => import('layouts/default'),
     children: [
       { path: '', component: () => import('pages/index-loggedout') },
-      { path: 'wallet', component: () => import('pages/wallet'), beforeEnter: logInCheck },
-      { path: 'settings', component: () => import('pages/settings'), beforeEnter: logInCheck },
-      { path: 'dashboard', component: () => import('pages/dashboard'), beforeEnter: logInCheck },
-      { path: 'workerproposals', component: () => import('pages/workerproposals'), beforeEnter: logInCheck },
-      { path: 'votecustodians', component: () => import('pages/votecustodians'), beforeEnter: logInCheck },
-      { path: 'profile/:accountname', component: () => import('pages/profile'), beforeEnter: logInCheck },
-      { path: 'constitution', component: () => import('pages/constitution'), beforeEnter: logInCheck },
-      { path: 'managecandidateship', component: () => import('pages/registercandidate'), beforeEnter: logInCheck },
-      { path: 'loggedout', component: () => import('pages/index-loggedout'), beforeEnter: logInCheck },
-      { path: 'custodiandashboard', component: () => import('pages/custodian-dashboard'), beforeEnter: custodianCheck }
+      { path: 'wallet', component: () => import('pages/wallet'), beforeEnter: Guards.logInCheck },
+      { path: 'settings', component: () => import('pages/settings'), beforeEnter: Guards.logInCheck },
+      { path: 'dashboard', component: () => import('pages/dashboard'), beforeEnter: Guards.logInCheck },
+      { path: 'workerproposals', component: () => import('pages/workerproposals'), beforeEnter: Guards.logInCheck },
+      { path: 'votecustodians', component: () => import('pages/votecustodians'), beforeEnter: Guards.logInCheck },
+      { path: 'profile/:accountname', component: () => import('pages/profile'), beforeEnter: Guards.logInCheck },
+      { path: 'constitution', component: () => import('pages/constitution'), beforeEnter: Guards.logInCheck },
+      { path: 'managecandidateship', component: () => import('pages/registercandidate'), beforeEnter: Guards.logInCheck },
+      { path: 'loggedout', component: () => import('pages/index-loggedout'), beforeEnter: Guards.logInCheck },
+      { path: 'custodiandashboard', component: () => import('pages/custodian-dashboard'), beforeEnter: Guards.custodianCheck }
     ]
   },
 
