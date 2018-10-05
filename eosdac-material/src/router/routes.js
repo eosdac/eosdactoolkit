05192e16ca9bb3c0c517ece27b8ab9d2c11719f5
Luke Stokes <luke.stokes@gmail.com>
2018-10-05 10:28:23 -0500

    Updating staging to match dev

--- eosdac-material/src/router/routes.js
+++ eosdac-material/src/router/routes.js
@@ -26,14 +26,14 @@ export default [
       { path: '', component: () => import('pages/index-loggedout') },
       { path: 'wallet', component: () => import('pages/wallet'), beforeEnter: Guards.logInCheck },
       { path: 'settings', component: () => import('pages/settings'), beforeEnter: Guards.logInCheck },
-      //{ path: 'dashboard', component: () => import('pages/dashboard'), beforeEnter: Guards.logInCheck },
-      //{ path: 'workerproposals', component: () => import('pages/workerproposals'), beforeEnter: Guards.logInCheck },
-      //{ path: 'votecustodians', component: () => import('pages/votecustodians'), beforeEnter: Guards.logInCheck },
-      //{ path: 'profile/:accountname', component: () => import('pages/profile'), beforeEnter: Guards.logInCheck },
+      { path: 'dashboard', component: () => import('pages/dashboard'), beforeEnter: Guards.logInCheck },
+      { path: 'workerproposals', component: () => import('pages/workerproposals'), beforeEnter: Guards.logInCheck },
+      { path: 'votecustodians', component: () => import('pages/votecustodians'), beforeEnter: Guards.logInCheck },
+      { path: 'profile/:accountname', component: () => import('pages/profile'), beforeEnter: Guards.logInCheck },
       { path: 'constitution', component: () => import('pages/constitution'), beforeEnter: Guards.logInCheck },
-      //{ path: 'managecandidateship', component: () => import('pages/registercandidate'), beforeEnter: Guards.logInCheck },
+      { path: 'managecandidateship', component: () => import('pages/registercandidate'), beforeEnter: Guards.logInCheck },
       { path: 'loggedout', component: () => import('pages/index-loggedout'), beforeEnter: Guards.logInCheck },
-      //{ path: 'custodiandashboard', component: () => import('pages/custodian-dashboard'), beforeEnter: Guards.custodianCheck }
+      { path: 'custodiandashboard', component: () => import('pages/custodian-dashboard'), beforeEnter: Guards.custodianCheck }
     ]
   },
 
