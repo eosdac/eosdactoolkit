2e952cac325a37355ccf02550d850d6a4e4145ea
kasperfish <kasperkasperfish@gmail.com>
2018-10-24 23:55:59 +0200

    sync

--- eosdac-material/src/components/msigcreator.vue
+++ eosdac-material/src/components/msigcreator.vue
@@ -1,21 +1,32 @@
 <template>
   <div class="row  bg-dark2 round-borders shadow-5" style="">
     <div class="col-md-6 col-xs-12 q-pa-md">
-      <q-input dark stack-label="Proposal name" v-model="msigtemplate.proposal_name" />
+      <div class="q-my-md">Proposal name</div>
+      <q-input dark  v-model="msigtemplate.proposal_name" />
       <div class="q-my-md">Expiration</div>
-      <q-datetime-picker minimal dark  v-model="msigtemplate.trx.expiration" type="date" />
+      <q-datetime-picker minimal dark color="positive" v-model="msigtemplate.trx.expiration" type="date" />
     </div>
     <div class="col-md-6 col-xs-12 q-pa-md ">
       <div class="column full-height justify-between">
         <div>
-          Add action
+<q-tabs>
+  <!-- Tabs - notice slot="title" -->
+
+  <q-tab slot="title" default name="tab-4"  label="Add Action" />
+  <q-tab slot="title" name="rawmsigtr" label="raw" />
+
+  <!-- Targets -->
+  <q-tab-pane class="bg-dark" name="tab-4">add action to msig</q-tab-pane>
+  <q-tab-pane class="bg-dark" name="rawmsigtr">
+    <pre>{{msigtemplate.trx}}</pre>
+  </q-tab-pane>
+</q-tabs>
         </div>
-        <div style="height:30px">
+        <div class="q-mt-md" style="height:30px">
           <q-btn class="float-right" label="propose" color="primary" @click="proposeMsig" />
         </div>
       </div>
     </div>
-    <!-- <pre>{{msigtemplate}}</pre> -->
     <Transaction ref="Transaction"  />
   </div>
 </template>
