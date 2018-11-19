5dc3f54ab5f1a14cccc54775930e1fec5f88f3e7
unknown <kasperkasperfish@gmail.com>
2018-11-19 21:02:57 +0100

    fix z-index back to top

--- eosdac-material/src/layouts/default.vue
+++ eosdac-material/src/layouts/default.vue
@@ -200,7 +200,7 @@
   <!-- depricated multimodal -->
   <!-- <MultiModal ref="Multi" /> -->
 
-  <q-btn v-back-to-top.animate="{offset: 500, duration: 200}" round color="primary" class="fixed-bottom-right animate-pop z-max" style="margin: 0 20px 15px 0;">
+  <q-btn v-back-to-top.animate="{offset: 500, duration: 200}" round color="primary" class="fixed-bottom-right animate-pop" style="margin: 0 20px 15px 0;">
     <q-icon name="keyboard_arrow_up" />
   </q-btn>
 
